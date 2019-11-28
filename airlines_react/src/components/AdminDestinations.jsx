93e1467da2c9a57894d56b573a97e7786eac8722
Phfa <phfa@outlook.com.au>
2019-11-28 21:09:35 +1100

    Templates added

---
+++ airlines_react/src/components/AdminDestinations.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class AdminCreateDestinations extends Component {
+  
+    render() { 
+        return (  
+            <div>
+                <h1>Destination comming soon</h1>
+            </div>
+        );
+    }
+}
+ 
+export default AdminCreateDestinations;
