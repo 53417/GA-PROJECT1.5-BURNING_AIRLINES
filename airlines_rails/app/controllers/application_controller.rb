eff743bfd9e4c64cf82d47d2b90fa9d4f6b1fa7e
Phfa <phfa@outlook.com.au>
2019-11-30 10:49:49 +1100

    created auth files

--- airlines_rails/app/controllers/application_controller.rb
+++ airlines_rails/app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    include Knock::Authenticable
+
+    # before_action :authenticate_user, except: [:index]
+    # Resources can now be secured by calling authenticate_user as a before_action inside your controllers
+
+    # You can access the current user in your controller with current_user.
+    # def index
+    #     if current_user
+    #       # do something
+    #     else
+    #       # do something else
+    #     end
+    #   end
+
 end
