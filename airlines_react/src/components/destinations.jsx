0af72d29296e6420e8dcec84fa1034e6570e32e9
KAY LEVIN <KS.KWANRUAN@GMAIL.com>
2019-11-30 11:53:55 +1100

    t3

---
+++ airlines_react/src/components/destinations.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Destinations extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <h1>Destinations coming soon</h1>
+         );
+    }
+}
+ 
+export default Destinations;
---
+++ airlines_react/src/components/displayFlights.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class DisplayFlight extends Component {
+    
+    render() { 
+        return ( 
+            <div>
+                <h1>Display Flights</h1>
+            </div>
+         );
+    }
+}
+ 
+export default DisplayFlight;
