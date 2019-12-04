d9929bd460fe9e73af271e459155df328eb98e8b
Phfa <phfa@outlook.com.au>
2019-12-04 21:47:57 +1100

    I dont know but it works

--- airlines_rails/config/application.rb
+++ airlines_rails/config/application.rb
@@ -12,6 +12,7 @@ module AirlinesRails
     config.load_defaults 6.0
 
     config.autoload_paths << Rails.root.join('lib')
+    # config.action_controller.default_protect_from_forgery = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
