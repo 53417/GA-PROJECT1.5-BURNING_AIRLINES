d6414c735cd3a60c8d9c53d4544c674ca5380534
Phfa <phfa@outlook.com.au>
2019-12-05 19:17:08 +1100

    undone session store changes back to normal

--- airlines_rails/config/initializers/session_store.rb
+++ airlines_rails/config/initializers/session_store.rb
@@ -1,1 +1,2 @@
-Rails.application.config.session_store :cookie_store, :active_record_store, domain:"http://localhost:3001"
+Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain:"*"
+# Rails.application.config.session_store :cookie_store, :active_record_store, domain:"http://localhost:3001"
